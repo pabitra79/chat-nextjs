108c0f7736687d0a66901d90e73422c6ece1e39b
Hamed Bahram <hamed.bahram@gmail.com>
2024-06-20 16:57:34 -0400

    Add chat page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-
-import Header from '@/components/header'
-import Footer from '@/components/footer'
 import { ThemeProvider } from '@/components/theme-provider'
 
 import './globals.css'
@@ -34,9 +31,7 @@ export default function RootLayout({
             defaultTheme='system'
             disableTransitionOnChange
           >
-            <Header />
             <main className='grow'>{children}</main>
-            <Footer />
           </ThemeProvider>
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
 export default function Home() {
   return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='text-3xl font-bold'>Realtime Chat & Video App</h1>
-      </div>
-    </section>
+    <main className='flex min-h-screen flex-col'>
+      <Header />
+
+      <section className='grow py-24'>
+        <div className='container'>
+          <h1 className='text-3xl font-bold'>Realtime Chat & Video App</h1>
+        </div>
+      </section>
+
+      <Footer />
+    </main>
   )
 }
