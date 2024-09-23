3393efa6a35772d069c49ae9d793d62b6ebea849
Hamed Bahram <hamed.bahram@gmail.com>
2024-09-23 08:46:11 -0400

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='py-4'>
       <div className='container flex flex-col items-center justify-between gap-x-3 gap-y-1 text-center text-sm text-muted-foreground sm:flex-row'>
-        <p>iServe &copy;{new Date().getFullYear()}. All rights reserved.</p>
+        <p>RealChat &copy;{new Date().getFullYear()}. All rights reserved.</p>
         <p className='text-xs'>
           Developed by{' '}
           <Link
